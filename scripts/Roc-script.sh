f07dfaef0e67e002351bf416b54ef20cd8a67780
zsj9418zsj <140706155+zsj9418zsj@users.noreply.github.com>
2024-12-01 10:28:47 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -26,6 +26,7 @@ function git_sparse_clone() {
 }
 
 # Alist & AdGuardHome & 集客无线AC控制器 & Lucky & AriaNg & 雅典娜LED控制
+git clone https://github.com/gngpp/luci-app-watchcat-plus.git package/luci-app-watchcat-plus
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
